c027f744e908406a6ddb6a0dbcf712cdfc7a5586
tinojose97 <tinojose97@gmail.com>
2020-02-13 18:01:00 +0800

    completed the first django app

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -5,6 +5,21 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Choice)
